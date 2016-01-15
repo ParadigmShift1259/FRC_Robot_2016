22cadfd312029d1512638c9d0bfd35edb507ede5
jds11235 <selfjoh15@gmail.com>
2016-01-15 11:31:19 -0600

    Project Created, Iterative Robot

---
+++ FRC2016Robot/spec.i
@@ -0,0 +1,1 @@
+#line 1 "C:/Users/selforsjd/workspace/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C"
---
+++ FRC2016Robot/src/Robot.cpp
@@ -0,0 +1,32 @@
+#include "WPILib.h"
+
+class Robot: public IterativeRobot
+{
+private:
+
+	void RobotInit()
+	{
+	}
+
+	void AutonomousInit()
+	{
+	}
+
+	void AutonomousPeriodic()
+	{
+	}
+
+	void TeleopInit()
+	{
+	}
+
+	void TeleopPeriodic()
+	{
+	}
+
+	void TestPeriodic()
+	{
+	}
+};
+
+START_ROBOT_CLASS(Robot)
