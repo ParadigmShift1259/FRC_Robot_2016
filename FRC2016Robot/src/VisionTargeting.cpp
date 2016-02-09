3f7a0dc23b45061bae6474b93aa248cfd7025f00
Developer <Developer@172.27.125.228>
2016-02-08 20:20:55 -0600

    Started Vision Targeting 2/8/16

---
+++ FRC2016Robot/src/VisionTargeting.cpp
@@ -0,0 +1,13 @@
+/*
+ * VisionTargeting.cpp
+ *
+ *  Created on: Feb 8, 2016
+ *      Author: Developer
+ */
+
+
+VisionTargeting::VisionTargeting(Drivetrain *drivetrain, OperatorInputs *operatorinputs)
+{
+	drive = drivetrain;
+	inputs = operatorinputs;
+}
---
+++ FRC2016Robot/src/VisionTargeting.h
@@ -0,0 +1,35 @@
+/*
+ * VisionTargeting.h
+ *
+ *  Created on: Feb 8, 2016
+ *      Author: Developer
+ */
+
+#ifndef SRC_VISIONTARGETING_H_
+#define SRC_VISIONTARGETING_H_
+
+#include "Drivetrain.h"
+#include "OperatorInputs.h"
+
+class VisionTargeting
+{
+public:
+	VisionTargeting(Drivetrain *drivetrain, OperatorInputs *operatorinputs);
+	~VisionTargeting();
+	double PickBestContour(double width1, double width2, double width3);
+	void DetectDirectionNeeded();
+	void Movement();
+
+	Drivetrain *drive;
+	OperatorInputs *inputs;
+
+
+protected:
+
+private:
+
+};
+
+
+
+#endif /* SRC_VISIONTARGETING_H_ */
