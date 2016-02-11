3d32f22fd9e4ef03fb959cd470725282dc9a6e26
Developer <Developer@172.27.125.227>
2016-02-10 18:59:15 -0600

    2/10/16 Temp code with some vision

--- FRC2016Robot/src/VisionTargeting.cpp
+++ FRC2016Robot/src/VisionTargeting.cpp
@@ -4,10 +4,20 @@
  *  Created on: Feb 8, 2016
  *      Author: Developer
  */
-
+#include "visiontargeting.h"
 
 VisionTargeting::VisionTargeting(Drivetrain *drivetrain, OperatorInputs *operatorinputs)
 {
 	drive = drivetrain;
 	inputs = operatorinputs;
+
+}
+VisionTargeting::~VisionTargeting()
+{
+
+}
+double VisionTargeting::PickBestContour(double width1, double width2, double width3)
+{
+	return 1.0;
+
 }
--- FRC2016Robot/src/VisionTargeting.h
+++ FRC2016Robot/src/VisionTargeting.h
@@ -24,6 +24,7 @@ public:
 	OperatorInputs *inputs;
 
 
+
 protected:
 
 private:
